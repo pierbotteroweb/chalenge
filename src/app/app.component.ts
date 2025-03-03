bf6a79188ee393c794003bd66b47528050fe7051
Pier Bottero <pierbotteroweb@gmail.com>
2025-03-03 19:22:40 -0300

    final-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PlaceholderService } from './placeholder.service';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
 export interface Users {
   user: string;
   email: string;
@@ -8,16 +9,25 @@ export interface Users {
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   constructor(private placeholderService: PlaceholderService) {}
   users: Users[] = [];
+  filterForm = new FormControl('');
 
   ngOnInit() {
+
+    this.filterForm.valueChanges.subscribe(change=>{
+      if(change){
+        this.users = this.users.filter(user=>user.user.includes(change))
+      }
+    })
+
     this.placeholderService.getUsers().subscribe((usersList: any) => {
+      console.log(usersList)
       this.users = usersList.map((user: { name: any; email: any }) => {
         return { user: user.name, email: user.email };
       });
